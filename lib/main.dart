e762de4416b54ef84d13d9a163d8d73d29721a1d
Tamaki094 <edri.alan.94@gmail.com>
2021-04-22 19:26:25 -0500

    1.-Se agrego enrutamiento. 2.-Se creo funcion para cargar iconos dinamicamnete. 3.-Se creo funciona para leer JSON y cargar menu a partir de los datos obtenidos

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
-import 'package:componentes/src/pages/home_temp.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/home_page.dart';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,8 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Componentes APP',
-        debugShowCheckedModeBanner: false,
-        home: HomePageTemp());
+      title: 'Componentes APP',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => HomePage(),
+        'alert': (BuildContext context) => AlertPage(),
+        'avatar': (BuildContext context) => AvatarPage()
+      },
+    );
   }
 }
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,60 @@
+import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:componentes/src/utils/icono_string_util.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+    // menuProvider.cargarData().then((opciones) {
+    //   print('_lista');
+    //   print(opciones);
+    // });
+    // return ListView(children: _listaItems());
+    //
+    //
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        print('builder');
+        print(snapshot.data);
+        return ListView(
+          children: _listaItems(snapshot.data, context),
+        );
+      },
+    );
+  }
+
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
+    final List<Widget> opciones = [];
+    data.forEach((opt) {
+      final widgetTmp = ListTile(
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {
+          ////opcion 1 navegacion
+          // final route = MaterialPageRoute(builder: (context) {
+          //   return AlertPage();
+          // });
+          // Navigator.push(context, route);
+
+          //opcion 2 navegacion
+          Navigator.pushNamed(context, opt['ruta']);
+        },
+      );
+
+      opciones..add(widgetTmp)..add(Divider());
+    });
+    return opciones;
+  }
+}
