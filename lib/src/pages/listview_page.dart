e1ab8f790da4129bee84e4b03b2b38b69fd9a6f3
Tamaki094 <edri.alan.94@gmail.com>
2021-05-15 21:40:25 -0500

    vacio

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ListaPage extends StatefulWidget {
@@ -6,33 +8,120 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
-  List<int> listaEnteros = [1, 2, 3, 4, 5, 6];
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> listaEnteros = new List();
+  int _ultimoItem = 0;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _agregar10();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        print('ultima pagina');
+        //_agregar10();
+        fetchData();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Listas"),
-      ),
-      body: _crearLista(),
-    );
+        appBar: AppBar(
+          title: Text("Listas"),
+        ),
+        body: Stack(
+          children: <Widget>[_crearLista(), _crearLoading()],
+        ));
   }
 
   Widget _crearLista() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: ListView.builder(
-        itemCount: listaEnteros.length,
-        itemBuilder: (BuildContext context, int posicion) {
-          final imagen = listaEnteros[posicion];
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FadeInImage(
-                image: NetworkImage('https://picsum.photos/id/$imagen/500/300'),
-                placeholder: AssetImage("assets/img/jar-loading.gif")),
-          );
-        },
+      child: RefreshIndicator(
+        onRefresh: obtenerPagina1,
+        child: ListView.builder(
+          controller: _scrollController,
+          itemCount: listaEnteros.length,
+          itemBuilder: (BuildContext context, int posicion) {
+            final imagen = listaEnteros[posicion];
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FadeInImage(
+                  image:
+                      NetworkImage('https://picsum.photos/id/$imagen/500/300'),
+                  placeholder: AssetImage("assets/img/jar-loading.gif")),
+            );
+          },
+        ),
       ),
     );
+  }
+
+  _agregar10() {
+    setState(() {
+      for (var i = 0; i < 10; i++) {
+        _ultimoItem++;
+        listaEnteros.add(_ultimoItem);
+      }
+    });
+  }
+
+  Future<Null> fetchData() async {
+    _isLoading = true;
+    setState(() {
+      final duration = new Duration(seconds: 5);
+      new Timer(duration,
+          respuestaHTTP); //si se pone parentesis la ejecutaria en se instante , manda la referencia
+    });
+  }
+
+  void respuestaHTTP() {
+    _isLoading = false;
+    _scrollController.animateTo(_scrollController.position.pixels + 100,
+        curve: Curves.fastOutSlowIn, duration: Duration(milliseconds: 250));
+    _agregar10();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  Widget _crearLoading() {
+    if (_isLoading) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[CircularProgressIndicator()],
+          ),
+          SizedBox(
+            height: 15.0,
+          )
+        ],
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  Future<Null> obtenerPagina1() async {
+    final duration = new Duration(seconds: 2);
+    new Timer(duration, () {
+      listaEnteros.clear();
+      _ultimoItem++;
+      _agregar10();
+    });
+
+    return Future.delayed(duration);
   }
 }
